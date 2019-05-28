adf205bf9a0769c6f4c0431ac500a845c18c627c
Gayah Karmo <gayahkarmo@gmail.com>
2019-05-28 17:32:41 -0400

    layout setup

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,14 @@
 var gameDiv = $("#game-body");
 
-var questionsArr = ["Longest win streak by any professional team?", "NBA player with the most championship rings?", "Most goals scored in a single hockey game", "Longest soccer goal made?"];
+var questionsObj = {
+    q1: "Longest win streak by any professional team?",
+    q2: "NBA player with the most championship rings?",
+    q3: "Most goals scored in a single hockey game",
+    q4: "Longest soccer goal made?"
+
+}
+
+// var questionsArr = ["Longest win streak by any professional team?", "NBA player with the most championship rings?", "Most goals scored in a single hockey game", "Longest soccer goal made?"];
 
 $(document).ready(function(){
 
@@ -8,15 +16,18 @@ $(document).ready(function(){
 
         $("#start").hide();
 
-        var questions = $("<p>");
+        var question1 = $("<p>" + questionsObj.q1 + "</p>"); 
+        var question2 = $("<p>" + questionsObj.q2 + "</p>");
+        var question3 = $("<p>" + questionsObj.q3 + "</p>");
+        var question4 = $("<p>" + questionsObj.q4 + "</p>");
         
-        for (var i = 0; i < questionsArr.length; i++) {
-            questions.text(questionsArr[i]);
-            
-        }
+        gameDiv.append(question1);
+        gameDiv.append(question2);
+        gameDiv.append(question3);
+        gameDiv.append(question4);
 
 
-        gameDiv.append(questions);
+        
 
     });
 
